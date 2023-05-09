75f9eaa8d98176a2fea74e3af76159f4ed3cde9a
facafile <filesimicevic@gmail.com>
2023-05-09 20:27:14 +0200

    finished lab 3

--- Raytracing/RayTracing_Java[2]/doc.cmd
+++
@@ -1,6 +0,0 @@
-pushd src
-if exist compile-list.txt del compile-list.txt
-for %%a in ( raytracing\* ) do echo %%a >> compile-list.txt
-echo Generating javadoc
-javadoc -d ../doc -sourcepath . @compile-list.txt
-popd
